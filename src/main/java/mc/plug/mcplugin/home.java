76e3bd709e1d80ca0b6f298897ade0c8bcb186e7
soup_mmm <maxtkenney@gmail.com>
2026-07-06 21:23:47 -0500

    Refactor home command for PvP and teleportation
    Refactor home command to use a timer map for PvP checks and improve teleportation logic.

--- src/main/java/mc/plug/mcplugin/home.java
+++ src/main/java/mc/plug/mcplugin/home.java
@@ -9,34 +9,30 @@ import org.bukkit.entity.Player;
 
 public class home implements CommandExecutor {
 
-
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
 
         if (!(sender instanceof Player player)) {
             return false;
         }
 
-        timers Timer = new timers();
-
-        boolean isPvp = Timer.pvp;
-        int timeLeft = Timer.pvptime;
-
-        if (isPvp == true) {
-            player.sendMessage("your are in pvp time left "+ timeLeft);
+        // Check if the player has an active timer in our map
+        if (timers.pvpTimers.containsKey(player.getUniqueId())) {
+            int timeLeft = timers.pvpTimers.get(player.getUniqueId());
+            player.sendMessage("You are in PvP! Time left: " + timeLeft + " S");
             return true;
         }
 
         Location spawn = player.getBedSpawnLocation();
 
         if (spawn == null) {
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(),"tp " + player.getName() + " 0 64 0" );
+            org.bukkit.World overworld = Bukkit.getWorlds().get(0);
+            player.teleport(new Location(overworld, 0, 64, 0));
             return true;
         }
 
         player.sendMessage(player.getName() + " is home");
-        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "tp " + player.getName() + " " + spawn.getBlockX() + " " + spawn.getBlockY() + " " + spawn.getBlockZ());
+        player.teleport(spawn);
 
         return true;
     }
