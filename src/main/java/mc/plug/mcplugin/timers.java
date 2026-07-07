30478c5592a165e54b06a0de0d519f9b2ff073aa
soup_mmm <maxtkenney@gmail.com>
2026-07-06 21:22:23 -0500

    Add PvP detection and combat timer functionality
    Implemented PvP detection with a timer system that notifies players when they are no longer in combat.

--- src/main/java/mc/plug/mcplugin/timers.java
+++ src/main/java/mc/plug/mcplugin/timers.java
@@ -1,21 +1,54 @@
 package mc.plug.mcplugin;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import java.util.HashMap;
+import java.util.UUID;
 
-public class timers {
+public class timers implements Listener {
 
-    boolean pvp = false;
-    Integer pvptime = 0;
+    // Stores <Player UUID, Seconds Left in Combat>
+    public static HashMap<UUID, Integer> pvpTimers = new HashMap<>();
 
-    public void updatePvpStatus() {
-        if (pvptime == 0) {
-            pvp = false;
+    public timers(JavaPlugin plugin) {
+        // Start a background loop that runs every 1 second (20 ticks)
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                // Loop through a copy of the keys to avoid errors while modifying the map
+                for (UUID uuid : new HashMap<>(pvpTimers).keySet()) {
+                    int secondsLeft = pvpTimers.get(uuid) - 1;
+
+                    if (secondsLeft <= 0) {
+                        pvpTimers.remove(uuid); // Time is up! Remove them from combat
+                        Player player = plugin.getServer().getPlayer(uuid);
+                        if (player != null) {
+                            player.sendMessage("You are no longer in combat.");
+                        }
+                    } else {
+                        pvpTimers.put(uuid, secondsLeft); // Update the map with lowered time
+                    }
+                }
+            }
+        }.runTaskTimer(plugin, 0L, 20L);
+    }
+
+    @EventHandler
+    public void updatePvpStatus(EntityDamageByEntityEvent event) {
+        if (event.getDamager() instanceof Player && event.getEntity() instanceof Player) {
+            Player damager = (Player) event.getDamager();
+            Player victim = (Player) event.getEntity();
+
+            // Putting a UUID in a HashMap automatically overwrites old values.
+            // This cleanly updates or resets their timer to 30 seconds.
+            pvpTimers.put(damager.getUniqueId(), 30);
+            pvpTimers.put(victim.getUniqueId(), 30);
         }
-
-
-
     }
 }
-//add pvp detection
 
 //W.I.P
