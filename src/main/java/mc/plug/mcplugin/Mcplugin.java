9e0f074183e21bf0f6a14390f47c21ef6f14b09e
soup_mmm <maxtkenney@gmail.com>
2026-07-21 02:53:24 -0500

    Initialize suggestion instance in onEnable method

--- src/main/java/mc/plug/mcplugin/Mcplugin.java
+++ src/main/java/mc/plug/mcplugin/Mcplugin.java
@@ -6,6 +6,8 @@ public final class Mcplugin extends JavaPlugin {
 
     @Override public void onEnable() {
 
+        suggestion suggestion = new suggestion();
+
         getLogger().info("mcplugin enabled!");
         getCommand("bd").setExecutor(new back_door());
         getCommand("home").setExecutor(new home());
